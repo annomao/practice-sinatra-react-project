76ea2d1878817a2c3c3c320b67e574f798babb52
annomao <omaoannkerubo@gmail.com>
2022-06-24 02:10:39 +0300

    add create table commands and model relationships

---
+++ app/models/report.rb
@@ -0,0 +1,4 @@
+class Report < ActiveRecord::Base
+  belongs_to :type
+  belongs_to :user
+end
--- db/migrate/20220623223737_create_reports.rb
+++ db/migrate/20220623223737_create_reports.rb
@@ -1,4 +1,12 @@
 class CreateReports < ActiveRecord::Migration[6.1]
   def change
+    create_table :reports do |t|
+      t.string :title
+      t.string :location
+      t.string :comment
+      t.integer :user_id
+      t.integer :type_id
+      t.timestamps
+    end
   end
 end
